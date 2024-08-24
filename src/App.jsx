f8cc995137bbaa09fb66c5cc5835467bbcc1303a
shitoletushar3132 <shitoletushar3132@gmail.com>
2024-08-24 22:18:51 +0530

    correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import Main from "./components/main";
+import Main from "./components/Main";
 import Footer from "./components/Footer";
 import { NotesContextProvider } from "./context/NotesContextProvider";
 
